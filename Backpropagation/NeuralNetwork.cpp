826f7cf2e46368c0a0d788061feedc0d674180cf
Orlando <porlando189@gmail.com>
2017-06-19 10:47:07 -0500

    Update NeuralNetwork.cpp

--- Backpropagation/NeuralNetwork.cpp
+++ Backpropagation/NeuralNetwork.cpp
@@ -1,24 +1,27 @@
 #include "NeuralNetwork.h"
 
-NeuralNetwork::NeuralNetwork(int nInputs, int hiddenLayerSize, int nHiddenLayers, int nOutputs, double (*activationFunction)(double),double (*gradientFunction)(double))
+NeuralNetwork::NeuralNetwork(int nInputs, vector<int> hiddenLayers, int nOutputs, double (*activationFunction)(double),double (*gradientFunction)(double))
 {
     mLayers.clear(); mNInputs=nInputs;mNOutputs=nOutputs;
 
-    mLayers.push_back(Layer (nInputs,0,hiddenLayerSize,activationFunction,gradientFunction));
+    if (hiddenLayers.size())
+        mLayers.push_back(Layer (nInputs,0,hiddenLayers.front(),activationFunction,gradientFunction));
+    else
+        mLayers.push_back(Layer (nInputs,0,nOutputs,activationFunction,gradientFunction));
 
-    for (int i=0;i<nHiddenLayers;++i){
-        if (i== 0)
-            mLayers.push_back(Layer (hiddenLayerSize,nInputs,hiddenLayerSize,activationFunction,gradientFunction));
-        else if(i<nHiddenLayers-1)
-            mLayers.push_back(Layer (hiddenLayerSize,hiddenLayerSize,hiddenLayerSize,activationFunction,gradientFunction));
-        else if (i==nHiddenLayers-1)
-            mLayers.push_back(Layer (hiddenLayerSize,hiddenLayerSize,nOutputs,activationFunction,gradientFunction));
+    if (hiddenLayers.size()>1){
+        for (unsigned int i=0;i<hiddenLayers.size()-1;++i)
+            mLayers.push_back(Layer (hiddenLayers[i],mLayers.back().mSize,hiddenLayers[i+1],activationFunction,gradientFunction));
     }
 
-    mLayers.push_back(Layer (nOutputs,hiddenLayerSize,0,activationFunction,gradientFunction));
+    if (hiddenLayers.size())
+        mLayers.push_back(Layer (hiddenLayers.back(),mLayers.back().mSize,nOutputs,activationFunction,gradientFunction));
+
+
+    mLayers.push_back(Layer (nOutputs,mLayers.back().mSize,0,activationFunction,gradientFunction));
 }
 
-int NeuralNetwork::computeOutput(vec input)
+int NeuralNetwork::computeOutput(vec& input)
 {
     mLayers.front().computeOutput(input);
 
@@ -28,93 +31,150 @@ int NeuralNetwork::computeOutput(vec input)
 
     double max=mLayers.back().mOutput[0];
     int pos=0;
-    for (int i=1;i<mLayers.back().mOutput.size();++i){
+    for (unsigned int i=1;i<mLayers.back().mOutput.size();++i){
         if(mLayers.back().mOutput[i]>max)
         {
             max=mLayers.back().mOutput[i];
             pos=i;
         }
     }
-    mLayers.back().mOutput.print("Output Final");
+
+   // mLayers.back().mOutput.print("Output Final");
     return pos;
 }
 
 
 
 
-void NeuralNetwork::backpropagation(vector<vec>& trainingSet)
+void NeuralNetwork::backpropagation(vector<vec>& trainingInput, vector<vec> &trainingOutput,double momentum)
 {
+    if (trainingInput.size()!=trainingOutput.size()){
+        cout<<"input not match with output in training set"<<endl;
+    }
 
     int iteration=0;
-    while(mError>0.0001 &&  iteration<1000){
+    mError=1000;
+
+    while(mError>0.001 &&  iteration<1000){
+        cout<<iteration<<endl;
         mError=0;
-        for (vec &example: trainingSet){
-            if (example.size() != (mNInputs + mNOutputs)){ cout<<"error input and neural network not match"<<endl;return;}
+        for (unsigned int i=0;i< trainingInput.size();++i){
+            if (trainingInput[i].size()== mNInputs && trainingOutput[i].size()==mNOutputs){
 
 
-            //FORDWARD
+                //FORDWARD
 
-            computeOutput(example.subvec(0,mNInputs-1));
+                computeOutput(trainingInput[i]);
 
+                //BACKWARD
+                vec error = trainingOutput[i] - mLayers.back().mOutput;
+                mLayers.back().mError = mLayers.back().mGradient % error;
 
+                mError+=sum(square(error));
 
-            //BACKWARD
+                for (int j =mLayers.size()-2;j>=0;--j){
 
+                    vec output=mLayers[j].mOutput;
+                    output.insert_rows(0,1);
+                    output.row(0)=1;
 
-            vec error = example.subvec(mNInputs,example.size()-1) - mLayers.back().mOutput;
-            mLayers.back().mError = mLayers.back().mGradient % error;
-            //cout<<"dfasdasd"<<endl;
+                    // gradient descent with momentum
 
-            mError+=sum(square(error));
+                    mLayers[j+1].mDeltaWeights=(momentum*mLayers[j+1].mDeltaWeights) + ((1-momentum)*mLearningRate*(mLayers[j+1].mError*trans(output)));
 
-            for (int i =mLayers.size()-2;i>=0;--i){
-
-                vec output=mLayers[i].mOutput;
-                output.insert_rows(0,1);
-                output.row(0)=1;
-
-                //mLayers[i+1].mWeights.print("w de la sgte "+to_string(mLayers[i+1].mWeights.n_rows)+"x"+to_string(mLayers[i+1].mWeights.n_cols));
-
-                //mLayers[i+1].mError.print("error de la sgte "+to_string(mLayers[i+1].mError.n_rows)+"x"+to_string(mLayers[i+1].mError.n_cols));
-               // output.print("mi salida "+to_string(output.n_rows)+"x"+to_string(output.n_cols));
-
-                //cout<<"rate "<<mLearningRate<<endl;
-                mat multi1=mLayers[i+1].mError*trans(output);
-                //multi1.print("salida*error");
-
-                mLayers[i+1].mWeights+=mLearningRate*mLayers[i+1].mError*trans(output);
+                    mLayers[j+1].mWeights+=mLayers[j+1].mDeltaWeights;
 
 
+                    mat pesos= trans(mLayers[j+1].mWeights);
 
-                //mLayers[i].mGradient.print("derivate");
+                    mLayers[j].mError=(pesos.submat(1,0,pesos.n_rows-1,pesos.n_cols-1)*mLayers[j+1].mError)%mLayers[j].mGradient;
 
-                //mLayers[i+1].mWeights.print("pesitos");
-
-                mat pesos= trans(mLayers[i+1].mWeights);
-
-                //pesos.print("trans de pesos");
-
-
-                //pesos.submat(1,0,pesos.n_rows-1,pesos.n_cols-1).print("sin bias");
-
-                mat mult=(pesos.submat(1,0,pesos.n_rows-1,pesos.n_cols-1))*mLayers[i+1].mError;
-
-                //mult.print("pesos *  error");
-
-
-                mLayers[i].mError=(pesos.submat(1,0,pesos.n_rows-1,pesos.n_cols-1)*mLayers[i+1].mError)%mLayers[i].mGradient;
-
-                //mLayers[i].mError.print("myerror");
-
+                }
 
             }
+            else{
+                cout<<"error input and neural network not match"<<endl;
+                return;
+            }
+
         }
 
-        mError*=0.5*(1.0/trainingSet.size());
-
-        //cout<<"_Current Error: "<<mError<<endl;
+        mError*=1.0/(2*trainingInput.size());
+        cout<<"error : "<<mError<<endl;
         ++iteration;
+    }
+}
+
+void NeuralNetwork::backpropagationMiniBatch(vector<vec>& trainingInput, vector<vec> &trainingOutput,int batchSize,double momentum)
+{
+    if (trainingInput.size()!=trainingOutput.size()){
+        cout<<"input not match with output in training set"<<endl;
     }
 
 
+    int iteration=0;
+    mError=1000;
+
+    while(mError>0.0001 &&  iteration<1000){
+        cout<<iteration<<endl;
+        mError=0;
+        for (unsigned int i=0;i< trainingInput.size();i+=batchSize){
+            unsigned int j=i;
+            for (;j < i+batchSize;++j){
+                if (j > trainingInput.size()-1) break;
+                if (trainingInput[j].size()== mNInputs && trainingOutput[j].size()==mNOutputs){
+                    //FORDWARD
+
+                    computeOutput(trainingInput[j]);
+
+                    //BACKWARD
+                    vec error = trainingOutput[j] - mLayers.back().mOutput;
+                    mLayers.back().mError = mLayers.back().mGradient % error;
+
+                    mError=sum(square(error));
+
+
+                    for (int k =mLayers.size()-2;k>=0;--k){
+
+                        vec output=mLayers[k].mOutput;
+                        output.insert_rows(0,1);
+                        output.row(0)=1;
+
+                        // gradient descent with momentum
+
+
+
+                        mLayers[k+1].mDeltaWeights=mLayers[k+1].mError*trans(output);
+
+                        mLayers[k+1].mDeltaWeightsSum+=mLayers[k+1].mDeltaWeights;
+
+                        mat pesos= trans(mLayers[k+1].mWeights+mLayers[k+1].mDeltaWeights);
+
+                        mLayers[k].mError=(pesos.submat(1,0,pesos.n_rows-1,pesos.n_cols-1)*mLayers[k+1].mError)%mLayers[k].mGradient;
+                    }
+
+
+                }
+                else{
+                    cout<<"error input and neural network not match"<<endl;
+                    return;
+                }
+            }
+
+            cout<<"batch size"<<j-i<<endl;
+            for (int k =mLayers.size()-2;k>=0;--k){
+                mLayers[k+1].mWeights+=mLearningRate*(1.0/(j-i))* mLayers[k+1].mDeltaWeightsSum;
+                mLayers[k+1].mDeltaWeightsSum*=0;
+            }
+        }
+
+        mError*=1.0/(2*trainingInput.size());
+        cout<<"error : "<<mError<<endl;
+
+        ++iteration;
+    }
 }
+
+
+
+
